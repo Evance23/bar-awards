145896ec3299e9b48efe016ace5f8f54b1ad7c13
Evance23 <evancebarracks01@gmail.com>
2022-02-19 02:16:02 +0300

    Add registration

--- awards/urls.py
+++ awards/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('awadz.urls'))
+    path('', include('awadz.urls')),
+    path('accounts/', include('registration.backends.simple.urls')),
     
 ]
